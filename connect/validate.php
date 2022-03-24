1799b7ef019ee817e0a3a8c7385cc32e8661ba4d
Revenge5432 <93908291+KubaROC@users.noreply.github.com>
2022-03-24 10:55:42 +0100

    changed folder structure

--- connect/validate.php
+++ connect/validate.php
@@ -22,7 +22,7 @@ if ($_SERVER["REQUEST_METHOD"]== "POST") {
           
         if(($user['adminname'] == $adminname) && 
             ($user['password'] == $password)) {
-                header("Location: ../adminpage.php");
+                header("Location: ../admin/adminpage.php");
                 $_SESSION['username']= $username; 
         }
         else {
